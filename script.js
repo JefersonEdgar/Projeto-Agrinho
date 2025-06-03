54a0b93e57687b3b91bf7450651b2a41f1a07025
JefersonEdgar <134607846+JefersonEdgar@users.noreply.github.com>
2025-06-03 16:27:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,5 +24,15 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
-
 });
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+botaoDeAcessibilidade.addEventListener('click', function (){
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+})
+const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
+ 
